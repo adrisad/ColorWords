c97cbf4c13392cdb4101fce7b0211133121a19df
adrisad <22082002andrademedina@gmail.com>
2025-08-12 16:34:49 -0400

    feat(backend): agregando metodos al controlador del modulo word

--- backend/src/module/words/word.controller.ts
+++ backend/src/module/words/word.controller.ts
@@ -26,4 +26,20 @@ export class WordController {
         const words = await this.serviceWord.getAllWords();
         return res.json(words);
     }
+    async editarWord(req: Request<{ id: string }, {}, word>, res: Response) {
+        const word = await this.serviceWord.editWord(Number(req.params.id), req.body)
+        return res.json(word)
+    }
+    async deleteWord(req: Request<{ id: string }, {}, word>, res: Response) {
+        const id = Number(req.params.id);
+        if (isNaN(id)) {
+            return res.status(400).json({ error: 'El id debe ser un número válido' });
+        }
+        const deletedWord = await this.serviceWord.deleteWord(id);
+        if (!deletedWord) {
+            return res.status(404).json({ error: 'Palabra no encontrada' });
+        }
+        return res.status(200).json({ message: 'Palabra eliminada correctamente' });
+    }
+
 }
