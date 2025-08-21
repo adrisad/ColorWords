c1ff8d71304eab2492509d8d40d393f0353cf67b
adrisad <22082002andrademedina@gmail.com>
2025-08-21 15:51:19 -0400

    feat(frontend): eliminando rastros del uso de render

--- frontend/src/app/Pages/home/home.ts
+++ frontend/src/app/Pages/home/home.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-
+  onButtonClick() {
+    console.log('FUNCIONA');
+    alert('Click detectado');
+  }
 }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './Pages/home/home';
+import { Reader } from './Pages/reader/reader';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: Home,
+    },
+    {
+        path: 'render',
+        component: Reader,
+    }
+];
